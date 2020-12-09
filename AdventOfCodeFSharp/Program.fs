46dfea29ca8bd43516b179f79f8b591506ebc8ba
Paul Wild <paulwild@gmail.com>
2020-12-09 18:01:41 +0000

    Day 8 - in f#

---
+++ AdventOfCodeFSharp/Program.fs
@@ -0,0 +1,14 @@
+﻿module AdventOfCodeFSharpFoo
+   
+open System
+open AdventOfCodeFSharp
+open AdventOfCodeFSharp.Day08
+
+[<EntryPoint>]
+let main argv =
+    let (part1Val,_) = part1
+    let part2Val = Seq.head part2
+    
+    printfn "Answer to Day 8 Part 1: %d" part1Val
+    printfn "Answer to Day 8 Part 2: %d" part2Val
+    0 // return an integer exit code
